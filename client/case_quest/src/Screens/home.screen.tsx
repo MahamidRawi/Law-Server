acbd693568b1a7b252e2e98c65db6b0701df8a54
Rawi <rawima2@gmail.com>
2023-11-28 22:53:20 +0200

    needs fixing

--- client/case_quest/src/Screens/home.screen.tsx
+++ client/case_quest/src/Screens/home.screen.tsx
@@ -1,9 +1,42 @@
 import React, {useState, useEffect} from "react";
+import { fetchHomePage } from "../actions/main/home.actions";
+
+interface UserCase {
+    // Define the structure of a user case here
+  }
+  
+  interface UserInfo {
+    // Define the structure of user info here
+  }
+
+interface HomePageResponse {
+    userCases: UserCase[];
+    userInfo: UserInfo;
+  }
 
 
 const Home = () => {
+    const [userCases, setUserCases] = useState<UserCase>([]);
+    const [userInfo, setUserInfo] = useState<UserInfo>({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+              const res = await fetchHomePage();
+              const data: HomePageResponse = (res as any).data; // Correct type assertion
+      
+              setUserCases(data.userCases);
+              setUserInfo(data.userInfo);
+            } catch (error) {
+              console.error("Error fetching data:", error);
+            }
+          };
+      
+          fetchData();
+      }, []);
     return (
         <div>
+            <button onClick={() => {console.log(userInfo)}}>Render</button>
             This is not the home page
         </div>
     )
--- client/case_quest/src/actions/main/home.actions.ts
+++ client/case_quest/src/actions/main/home.actions.ts
@@ -1,13 +1,22 @@
 import { useContext } from 'react';
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { AuthContext } from '../../Providers/auth.provider';
+import config from '../../config';
 
+const fetchHomePage = () => {
+    return new Promise(async (resolve, reject) => {
+        const { user, logout } = useContext(AuthContext);
+        const token = localStorage.getItem('user_token');
 
-const fetchMyCases = () => {
-    const { user, logout } = useContext(AuthContext);
-    const token = localStorage.getItem('user_token');
+        if (!user || !token) return logout();
 
-    if (!user || !token) return logout();
-
-    
+        try {
+            const res = await axios.get(config.API_BASE_URL + '/main/fetchHomePage', {headers: {'x-access-token': token}}) as AxiosResponse;
+            return resolve({success: true, info: res.data});
+        } catch (err) {
+            return logout();
+        }
+});
 }
+
+export {fetchHomePage}
