2a0f6aec12127ed7591dc20b6312f9c2ed344e28
Rawi <rawima2@gmail.com>
2023-12-31 00:07:11 +0200

    adjust max value

--- client/case_quest/src/Screens/wallet/wallet.transfer.tsx
+++ client/case_quest/src/Screens/wallet/wallet.transfer.tsx
@@ -1,28 +1,33 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import { ActivityIncicator } from '../../RC/acitivity.incdicator';
 import { Button } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
 import './wallet.css'
+import { balanceParser } from '../../helper/res.helper';
+import { getWallet } from '../../actions/main/wallet.actions';
+import { AuthContext } from '../../Providers/auth.provider';
 
 interface TransferProps {
     walletNumber?: string
 }
 
 const TransferScreen: React.FC<TransferProps> = ({walletNumber}) => {
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const {logout} = useContext(AuthContext);
     const [recipientWallet, setRecipientWallet] = useState(walletNumber || '');
     const [amount, setAmount] = useState('');
     const [reason, setReason] = useState('');
+    const [wallet, setWallet] = useState<any>({});
 
-    // Mock reasons for the select dropdown
-    const reasons = ["Mock Reason 1", "Mock Reason 2", "Mock Reason 3"];
+    const reasons = ["Service Payment", "Gift", "Other"];
 
-    // Handle form submission
+    useEffect(() => {
+        getWallet().then(res => setWallet(res.wallet)).catch(err => err.AR ? logout() : alert('An Error Has Occured'));
+        return setLoading(false);
+    })
+
     const handleSubmit = (event: { preventDefault: () => void; }) => {
         event.preventDefault();
-        // Process the form data here
-        console.log({ recipientWallet, amount, reason });
-        // Add your logic to handle the transfer
     };
     return (
         loading ? <ActivityIncicator fullScreen /> : (
@@ -44,7 +49,7 @@ const TransferScreen: React.FC<TransferProps> = ({walletNumber}) => {
                     <Form.Control
                         type="number"
                         step="1"
-                        max="200"
+                        max={wallet.balance/1.02}
                         className='mb-4'
                         value={amount}
                         onChange={e => setAmount(e.target.value)}
@@ -58,6 +63,8 @@ const TransferScreen: React.FC<TransferProps> = ({walletNumber}) => {
                         {reasons.map((r, idx) => <option key={idx} value={r}>{r}</option>)}
                     </Form.Control>
                 </Form.Group>
+                
+                <h5 className='mb-4'>Total + 2% fee : {balanceParser(1.02 * Number(amount))}</h5>
 
                 <Button variant="primary" type="submit">Transfer</Button>
             </Form>
